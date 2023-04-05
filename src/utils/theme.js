37916c990f7afb43afdf8d83008cefa5644e3140
MissaelVarela <missaelyakitori@gmail.com>
2023-04-05 14:23:27 -0700

    add utils files, data/img images, assets/img images

---
+++ src/utils/theme.js
@@ -0,0 +1,29 @@
+const theme = {
+    colors: {
+        primary: "#0066FF",
+        secundary: "#97BFFF",
+        tertiary: "#BDD6F6",
+        quaternary: "#ECF4FF",
+        light: "#FFFFFF",
+        lightDark: "#F5F5F5",
+        darkLight: "#DADADA",
+        dark: "#3D3D3D",
+    },
+    fonts: {
+        main: "System",
+    },
+    fontSizes: {
+        text: 14,
+        title: 20,
+        subtitle: 16,
+        buttonText: 14
+    },
+    fontWeights: {
+        regular: "400",
+        semiBold: "600",
+        bold: "700",
+    }
+
+}
+
+export default theme;
